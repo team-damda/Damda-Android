81a4d74f361832ae6130af33be794d4d1cb728a9
expresshihway <03wow22@gmail.com>
2021-07-21 00:07:58 +0900

    [fix] test commit & push

--- app/src/main/java/com/etwoitwo/damda/MainActivity.kt
+++ app/src/main/java/com/etwoitwo/damda/MainActivity.kt
@@ -2,12 +2,10 @@ package com.etwoitwo.damda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("yg_test", "me")
     }
 }
