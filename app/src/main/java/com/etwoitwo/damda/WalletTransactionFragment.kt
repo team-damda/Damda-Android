ce507a0ac46ab64a70e2687ba4683dbd8833b8b9
Jin Lee <leejin21c@gmail.com>
2021-07-26 23:03:37 +0900

    feat(xml): 지갑페이지 거래 내역 탭 부분 추가

--- app/src/main/java/com/etwoitwo/damda/WalletTransactionFragment.kt
+++ app/src/main/java/com/etwoitwo/damda/WalletTransactionFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HoldingStockNone.newInstance] factory method to
+ * Use the [WalletTransactionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HoldingStockNone : Fragment() {
+class WalletTransactionFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class HoldingStockNone : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_holding_stock_none, container, false)
+        return inflater.inflate(R.layout.fragment_wallet_transaction, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class HoldingStockNone : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HoldingStockNone.
+         * @return A new instance of fragment WalletTransactionFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HoldingStockNone().apply {
+            WalletTransactionFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
