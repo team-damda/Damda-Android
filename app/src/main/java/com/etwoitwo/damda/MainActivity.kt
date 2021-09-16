abf76f0e99cdfb6df522118d6776d366870799dc
JinLee <41279487+leejin21@users.noreply.github.com>
2021-09-16 18:47:08 +0900

    [Feature/socket] socket 밑 작업(기능 구동 주석처리) (#13)
    * feat(bottom nav bar): bottom navigation bar 생성 및 그에 따른 작업
    
    * feat(socket): socket 기본, 메인 화면에 밑작업(동작 아직 안하도록 주석처리)

--- app/src/main/java/com/etwoitwo/damda/MainActivity.kt
+++ app/src/main/java/com/etwoitwo/damda/MainActivity.kt
@@ -2,10 +2,55 @@ package com.etwoitwo.damda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+//    lateinit var mSocket: Socket
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(MainStockFragment())
+
+        bottomNavigationView = findViewById(R.id.bottomNavBar)
+        initNavigationBar()
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun initNavigationBar() {
+        bottomNavigationView.setOnItemSelectedListener {
+            return@setOnItemSelectedListener when (it.itemId) {
+                R.id.item_bottomnav_main -> {
+                    replaceFragment(MainStockFragment())
+                    true
+                }
+                R.id.item_bottomnav_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+                R.id.item_bottomnav_wallet -> {
+                    replaceFragment(WalletFragment())
+                    true
+                }
+                R.id.item_bottomnav_more -> {
+                    replaceFragment(MoreFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+
 }
