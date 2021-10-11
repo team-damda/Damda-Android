fb0efc3ca9762192028f40f9fd85e0b75593da29
Jin Lee <leejin21c@gmail.com>
2021-10-11 21:12:45 +0900

    feat(transaction): 전체, 매수, 매도 버튼 (라디오그룹)에 리스너 추가

--- app/src/main/java/com/etwoitwo/damda/feature/wallet/WalletTransactionFragment.kt
+++ app/src/main/java/com/etwoitwo/damda/feature/wallet/WalletTransactionFragment.kt
@@ -7,9 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.etwoitwo.damda.R
 import com.etwoitwo.damda.databinding.FragmentWalletTransactionBinding
+import com.etwoitwo.damda.model.dataclass.StockData
 
 /* [설계]
 
@@ -20,11 +24,23 @@ import com.etwoitwo.damda.databinding.FragmentWalletTransactionBinding
 받은 거래 내역 리스트에서 버튼을 클릭하면 매수/매도 클릭하여 보여줌.
 이때 child fragment 교체해주기(데이터 있으면 -> wallet_transaction_list로, 데이터 없으면 -> wallet_transaction_none으로)
 
+[
+    {
+        date: "21/5/6(수)",
+        dayProfitLoss: -5000,
+        transList: [
+            {marketType, stockId, stockName, currentPrice, transType, transPrice, transAmount, transTime, transProfitLoss, transProfitLossRate}
+            , ...
+        ]
+    }
+]
 */
 
 class WalletTransactionFragment : Fragment() {
     private var _binding: FragmentWalletTransactionBinding? = null
     private val binding get() = _binding!!
+    private var selectedButton: String = "전체"
+    private val data: StockData? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,10 +54,11 @@ class WalletTransactionFragment : Fragment() {
         val monthSpinnerArray:Array<String> = arrayOf("21/5월", "20/11월")
         val adapter = ArrayAdapter(requireContext(), R.layout.layout_wallet_spinneritem, monthSpinnerArray)
         spinner.adapter = adapter
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
+
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
@@ -51,6 +68,16 @@ class WalletTransactionFragment : Fragment() {
                 Log.d("spinner11 onItemClicked", "${spinner.getItemAtPosition(position)}")
             }
 
+        }
+
+        //* 라디오 버튼에 onclicklistener 설정해서 골라진 것 저장하기
+        /* TODO 클릭되자마자 저장된 데이터 배열에서 필터링해서 화면에 보여주기 */
+        binding.btnWalletRadiogroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.btn_wallet_filterall -> Log.d("라디오 그룹", "filter all")
+                R.id.btn_wallet_filterbuy -> Log.d("라디오 그룹", "filter buy")
+                R.id.btn_wallet_filtersell -> Log.d("라디오 그룹", "filter sell")
+            }
         }
 
         return binding.root
