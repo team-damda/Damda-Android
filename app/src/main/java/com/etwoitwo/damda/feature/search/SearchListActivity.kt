e60be671ec3f4fd81a46195fd9e417a011c5a550
tnsy <soo5717@sookmyung.ac.kr>
2021-10-14 07:42:47 +0900

    feat: SearchListActivity.kt 뷰바인딩 구현

--- app/src/main/java/com/etwoitwo/damda/feature/search/SearchListActivity.kt
+++ app/src/main/java/com/etwoitwo/damda/feature/search/SearchListActivity.kt
@@ -3,10 +3,14 @@ package com.etwoitwo.damda.feature.search
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.etwoitwo.damda.R
+import com.etwoitwo.damda.databinding.ActivitySearchListBinding
 
 class SearchListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySearchListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_list)
+        binding = ActivitySearchListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
