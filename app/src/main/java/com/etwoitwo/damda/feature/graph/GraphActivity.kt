70d8b0e8ca76c96f2a3ddf898c783b7a867cce84
박윤경 <54926663+expresshighway@users.noreply.github.com>
2021-10-14 12:58:54 +0900

    Feature/graph (#28)
    * style: style XML 수정
    
    * style: XML Button, backgroundTint="@null" 추가
    
    * style: XML Button padding 추가, background 수정
    
    * style: XML Button padding 추가, background 수정
    
    * style: bg_wallet_button 초기화
    
    * fix: .gitgnore, misc 설정 변경
    
    * style: misc conflic
    
    * style: chart 문자열 @string/chart로 변경
    
    * .gitignore is now working
    
    * style: buying, selling xml 변경
    
    * feat: add sellingActivity keypad
    
    * feat: 매도하기 SeliingActivity 안드로이드 기능 동작 구현 (서버 연결 및 매도 연결 X)
    
    * feat: 매수하기 다이얼로그 연결, 매도하기 다이얼로그 연결, 화면 전환 완료
    
    * feat: 그래프 MPAndroid 커스텀, 페이지 연결
    
    * bug: graph fragment fixed
    
    * bug:bug fix, 페이지 연결 수정

---
+++ app/src/main/java/com/etwoitwo/damda/feature/graph/GraphActivity.kt
@@ -0,0 +1,71 @@
+package com.etwoitwo.damda.feature.graph
+
+import android.app.PendingIntent.getActivity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.etwoitwo.damda.R
+import com.etwoitwo.damda.databinding.ActivityDetailInfoBinding
+import com.etwoitwo.damda.feature.search.SearchListActivity
+
+
+private lateinit var binding: ActivityDetailInfoBinding
+
+class GraphActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFragment()
+        binding.buttonOneDay.setOnClickListener{
+
+            setFragment()
+            Log.d("tag", "setOnClickListener")
+        }
+
+        binding.buttonOneWeek.setOnClickListener{
+            setFragmentOneWeek()
+        }
+
+        binding.textViewDivision.setText("코스닥") // 서버 연결
+        binding.textViewStockCode.setText("078930") // 서버 연결
+        binding.textViewStockName.setText("GS") // 서버 연결
+        binding.textViewStockMoney.setText("45,000")
+
+        binding.textViewIncreaseMoney.setText("▲ 1,000") // 서버 연결
+        binding.textViewIncreaseRate.setText("+3.3") // 서버 연결
+
+        binding.buttonBuying.setOnClickListener{
+            val buyingIntent = Intent(this, BuyingActivity::class.java)
+            startActivity(buyingIntent)
+        }
+        binding.buttonSelling.setOnClickListener{
+            val sellingIntent = Intent(this, SellingActivity::class.java)
+            startActivity(sellingIntent)
+        }
+        binding.buttonBack.setOnClickListener{
+            val Intent = Intent(this, SearchListActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+
+    private fun setFragment(){
+        val transaction = supportFragmentManager.beginTransaction()
+            .replace(R.id.flame_layout_graph_shape, ChartFragment1())
+            .addToBackStack(null)
+        transaction.commit()
+
+    }
+    private fun setFragmentOneWeek(){
+        val transaction = supportFragmentManager.beginTransaction()
+            .replace(R.id.flame_layout_graph_shape, ChartFragment2())
+            .addToBackStack(null)
+        transaction.commit()
+    }
+
+}
---
+++ app/src/main/java/com/etwoitwo/damda/feature/graph/SellingActivity.kt
@@ -0,0 +1,156 @@
+package com.etwoitwo.damda.feature.graph
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.etwoitwo.damda.databinding.ActivitySellingBinding
+
+private lateinit var binding: ActivitySellingBinding
+
+class SellingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySellingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var BinderRecentMoney = binding.textViewRecentMoney
+        var BinderInputStock = binding.editTextInputStock
+        BinderRecentMoney.setText("15000")
+
+        var recentMoney : Int  = Integer.parseInt(BinderRecentMoney.text.toString())
+        var totalMoney = 0
+        var inputStock = 0
+
+        var BinderResultMoney = binding.textViewResultMoney
+        BinderResultMoney.setText("+500000")
+
+        var storedStock = 100 // 현재 내가 가진 주식 개수 적어주기
+        var restStock = 0
+        var lack = ""
+
+        // 주의 개수를 입력하면 실시간으로 총 가격이 변함
+        BinderInputStock.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString() == ""){
+                    inputStock = 0
+                    totalMoney = 0
+                    lack="남음"
+                    restStock = storedStock
+                    binding.textViewTotalMoney.setText("0")
+                }
+                else {
+                    inputStock = Integer.parseInt(s.toString())
+                    totalMoney = inputStock * recentMoney
+                    binding.textViewTotalMoney.setText(totalMoney.toString())
+
+                    if (storedStock > inputStock) {
+                        restStock = storedStock - inputStock
+                        lack = "남음"
+                        changeColor("#000000")
+
+                    }
+                    else {
+                        restStock = inputStock - storedStock
+                        lack = "부족"
+                        changeColor("#e6504f")
+                    }
+                }
+                binding.textViewRestStock.setText(restStock.toString())
+                binding.textViewLack.setText(lack)
+            }
+        })
+
+        addNumberKeyboard()
+
+        binding.buttonMaxSelling.setOnClickListener{
+            binding.editTextInputStock.setText(storedStock.toString())
+            binding.textViewRestStock.setText("0")
+            totalMoney = inputStock * recentMoney
+            binding.textViewTotalMoney.setText(totalMoney.toString())
+            binding.textViewLack.setText(lack)
+            changeColor("#000000")
+        }
+        binding.buttonSelling.setOnClickListener{
+            binding.buttonSelling.setOnClickListener {
+                val dialog = SellingCustomDialog()
+                dialog.show(supportFragmentManager, "SellingCustomDialog")
+
+            }
+        }
+        binding.buttonBack.setOnClickListener{
+            val Intent = Intent(this, GraphActivity::class.java)
+            startActivity(Intent)
+        }
+    }
+
+    fun changeColor(colorString: String){
+        binding.textViewMoneyUnit3.setTextColor(Color.parseColor(colorString))
+        binding.textViewLack.setTextColor(Color.parseColor(colorString))
+        binding.textViewLeftParentheses2.setTextColor(Color.parseColor(colorString))
+        binding.textViewRightParentheses2.setTextColor(Color.parseColor(colorString))
+        binding.textViewRestStock.setTextColor(Color.parseColor(colorString))
+    }
+
+    fun addNumberKeyboard (){
+        binding.buttonZero.setOnClickListener{
+            binding.editTextInputStock.append("0")
+        }
+
+        binding.buttonOne.setOnClickListener{
+            binding.editTextInputStock.append("1")
+        }
+
+        binding.buttonTwo.setOnClickListener{
+            binding.editTextInputStock.append("2")
+        }
+
+        binding.buttonThree.setOnClickListener{
+            binding.editTextInputStock.append("3")
+        }
+
+        binding.buttonFour.setOnClickListener{
+            binding.editTextInputStock.append("4")
+        }
+
+        binding.buttonFive.setOnClickListener{
+            binding.editTextInputStock.append("5")
+        }
+
+        binding.buttonSix.setOnClickListener{
+            binding.editTextInputStock.append("6")
+        }
+
+        binding.buttonSeven.setOnClickListener{
+            binding.editTextInputStock.append("7")
+        }
+
+        binding.buttonEight.setOnClickListener{
+            binding.editTextInputStock.append("8")
+        }
+
+        binding.buttonNine.setOnClickListener{
+            binding.editTextInputStock.append("9")
+        }
+
+        binding.buttonAllDelete.setOnClickListener{
+            binding.editTextInputStock.setText(null)
+        }
+        
+        binding.buttonBackspace.setOnClickListener{
+                binding.editTextInputStock.setText(binding.editTextInputStock.text.toString().substring(0, binding.editTextInputStock.text.toString().length-1))
+        }
+    }
+}
